97e2f24874e17175e710dad229f6a713a4925091
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-03-25 16:45:49 -0500

    feat(expense): add repository

---
+++ src/main/java/pe/com/carlosh/tallyapi/expense/ExpenseRepository.java
@@ -0,0 +1,12 @@
+package pe.com.carlosh.tallyapi.expense;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    Page<Expense> findByUserId(Long userId, Pageable pageable);
+    Page<Expense> findByUserIdAndCategoryId(Long userId, Long categoryId, Pageable pageable);
+}
