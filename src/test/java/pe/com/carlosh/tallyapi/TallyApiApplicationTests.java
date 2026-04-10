e989e03875776549196891a0d2cc2c08dba6e81a
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-04-10 00:44:42 -0500

    test: configure H2 and test profile

--- src/test/java/pe/com/carlosh/tallyapi/TallyApiApplicationTests.java
+++ src/test/java/pe/com/carlosh/tallyapi/TallyApiApplicationTests.java
@@ -2,8 +2,10 @@ package pe.com.carlosh.tallyapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TallyApiApplicationTests {
 
     @Test
